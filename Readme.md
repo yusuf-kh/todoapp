ed743a5196b938617763e373fb23289786cf450e
aashit.s <aashit.s@media.net>
2020-07-20 18:21:13 +0530

    startup code

---
+++ Readme.md
@@ -0,0 +1,27 @@
+## Steps to work on local
+```
+1. Clone the project.
+2. RUN "npm install"
+3. start the project using command "npm run start"
+4. go to http://localhost:3000/ on your browser
+```
+
+## Directory Structure
+```
+.
+├── app.js
+├── bin
+│   └── www
+├── package.json
+├── public
+│   ├── images
+│   ├── js
+│       └── todo.js
+│   └── css
+│       └── style.css
+├── routes
+│   ├── index.js
+└── views
+    ├── error.html
+    ├── index.html
+```
---
+++ app.js
@@ -0,0 +1,41 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+
+global.STATUS_CODE = require('./constants/httpStatusCodes');
+
+let app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(STATUS_CODE.NOT_FOUND));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = req.app.get('env') === 'development' ? err.message : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || STATUS_CODE.INTERNAL_SERVER_ERROR);
+    res.sendFile(__dirname + '/views/error.html');
+});
+
+module.exports = app;
